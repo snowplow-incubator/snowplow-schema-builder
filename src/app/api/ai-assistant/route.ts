56f5b7d40d86b6a53d04d0d8b3ae773333764840
Matúš Tomlein <matus.tomlein@gmail.com>
2023-11-02 11:22:14 +0100

    Update prompt

--- src/app/api/ai-assistant/route.ts
+++ src/app/api/ai-assistant/route.ts
@@ -4,22 +4,21 @@ import OpenAI from "openai";
 // configuration
 const baseURL = "https://api.endpoints.anyscale.com/v1";
 const apiKey = process.env.OPENAI_API_KEY;
-const model = "meta-llama/Llama-2-7b-chat-hf";
+const model = "meta-llama/Llama-2-13b-chat-hf";
 
 const openai = new OpenAI({ baseURL, apiKey });
 
 function createPrompt(useCaseDescription: string, reports: string[]) {
-  return `Please suggest a tracking design addressing the following use case: ${useCaseDescription}
-Your tracking design should consist of a set of events and reusable entities.
+  return `I want to design events and entities addressing the following use case: ${useCaseDescription}
 
 Events are actions that the user does in the app. Event names should be written in camel case following a verb_noun structure, for example add_to_cart, or rate_app.
 
 Entities are objects that are common across multiple events and add additional context. Entities should be written as a single word, for example user, or product.
 
-I am interested in using the dimensions to create the following reports:
+I am interested in using them to create the following reports:
 - ${reports.join("\n- ")}
 
-The answer should consist of two JSON lists - one for events and one for entities. Each list should contain objects with "name", "description" and "properties" fields. The properties should be a list of objects with "name", "description" and "type" fields.`
+Please reply with two valid JSON lists - one for events and one for entities. Each list should contain objects with "name", "description" and "properties" fields. The properties should be a list of objects with "name", "description" and "type" fields.`
 }
 
 export async function POST(req: Request) {
